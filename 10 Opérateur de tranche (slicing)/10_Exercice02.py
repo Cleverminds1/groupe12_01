195717046eb8cae064fe53e0f18daa5333ba0405
Christian enanga <clever01162003@gmail.com>
2025-07-19 15:41:53 +0200

    clever

--- 10 Opérateur de tranche (slicing)/10_Exercice02.py
+++
@@ -1,6 +0,0 @@
-entree = input("Entrez des éléments séparés par des espaces : ")
-liste = entree.split()
-
-liste_inversee = liste[::-1]
-
-print(f"Liste inversée : {liste_inversee}")
--- 8 Itération par élément/08_Exercice02.py
+++
@@ -1,9 +0,0 @@
-'''
-Énoncé : 
-Demander une liste d’éléments et afficher chaque élément avec son indice.
-'''
-entree = input("Entrez des éléments : ")
-liste = entree.split()
-
-for i, elem in enumerate(liste):
-    print(f"Indice {i} : {elem}")
